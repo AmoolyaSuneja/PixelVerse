2d39ce9de1036e42b5f73bd312f1b20b1cfabe77
Amoolya <asuneja007@gmail.com>
2026-08-15 12:14:43 +0530

    setup express http server application boilerplate

---
+++ apps/http/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { router } from "./routes/v1";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    origin: ["https://pixelverse.stelliform.xyz", "http://localhost:5173"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use("/api/v1", router);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
